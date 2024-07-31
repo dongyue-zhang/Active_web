59e7277d126eead7bd9d0b3cafa430be76ef4fd7
Dongyue Zhang <zhangdongyue22@gmail.com>
2024-07-31 14:55:28 -0400

    test actions

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    production: true,
+    googleMapKey: process.env['GOOGLE_MAP_KEY'],
+    basePath: '/apps/active/api',
+    facilityPath: '/facilities',
+    typePath: '/types',
+    activityPath: '/activities',
+    categoryPath: '/categories',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    production: false,
+    googleMapKey: process.env['GOOGLE_MAP_KEY'],
+    basePath: 'https://cors-anywhere.herokuapp.com/https://emrahkinay.com/apps/active/api/v2/ottawa',
+    facilityPath: '/facilities',
+    typePath: '/types',
+    activityPath: '/activities',
+    categoryPath: '/categories',
+};
